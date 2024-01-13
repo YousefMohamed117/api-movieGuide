14a8ecde58e73a48b37c2118a274d422c4320859
YousefMohamed117 <114279536+YousefMohamed117@users.noreply.github.com>
2024-01-13 14:15:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,59 @@
-let showContainer = document.querySelector(".result");
-let input = document.querySelector("input");
-let button = document.querySelector("button");
-let listContainer = document.getElementById('suggestions');
+//Initial References
+let movieNameRef = document.getElementById("movie-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
 
-let apiKey = "e62be596";
-function displayWords(value) {
-  input.value = value;
-  removeElements();
-}
-
-function removeElements() {
-  listContainer.innerHTML = "";
-}
-
-input.addEventListener("keyup", async () => {
-  removeElements();
-  if (input.value.length < 3) {
-    return false;
+//Function to fetch data from API
+let getMovie = () => {
+  let movieName = movieNameRef.value;
+  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+  //If input field is empty
+  if (movieName.length <= 0) {
+    result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
   }
-
-  const url = `https://www.omdbapi.com/?s=${input.value}&apikey=${apiKey}`;
-
-  const response = await fetch(url);
-  const jsonData = await response.json();
-
-  jsonData.Search.forEach((result) => {
-    let title = result.Title;
-    let div = document.createElement("div");
-    div.style.cursor = "pointer";
-    div.classList.add("autocomplete-items");
-    div.setAttribute("onclick", "displayWords('" + title + "')");
-    let word = "<b>" + title.substr(0, input.value.length) + "</b>";
-    word += title.substr(input.value.length);
-    div.innerHTML = `<p class="item">${word}</p>`;
-    listContainer.appendChild(div);
-  });
-});
-
-button.addEventListener("click", async () => {
-  if (input.value.trim().length < 1) {
-    alert("Input cannot be blank");
+  //If input field is NOT empty
+  else {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        //If movie exists in database
+        if (data.Response == "True") {
+          result.innerHTML = `
+            <div class="info">
+                <img src=${data.Poster} class="poster">
+                <div>
+                    <h2>${data.Title}</h2>
+                    <div class="rating">
+                        <img src="star-icon.svg">
+                        <h4>${data.imdbRating}</h4>
+                    </div>
+                    <div class="details">
+                        <span>${data.Rated}</span>
+                        <span>${data.Year}</span>
+                        <span>${data.Runtime}</span>
+                    </div>
+                    <div class="genre">
+                        <div>${data.Genre.split(",").join("</div><div>")}</div>
+                    </div>
+                </div>
+            </div>
+            <h3>Plot:</h3>
+            <p>${data.Plot}</p>
+            <h3>Cast:</h3>
+            <p>${data.Actors}</p>
+            
+        `;
+        }
+        //If movie does NOT exists in database
+        else {
+          result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
+        }
+      })
+      //If error occurs
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+      });
   }
-  showContainer.innerHTML = "";
-  const url = `https://www.omdbapi.com/?t=${input.value}&apikey=${apiKey}`;
-
-  const response = await fetch(url);
-  const jsonData = await response.json();
-
-  if (jsonData.Response === "True") {
-    showContainer.innerHTML = `<div class="card-container">
-        <div class="container-character-image">
-        <img src="${jsonData.Poster}"/></div>
-        <div class="character-name">${jsonData.Title}</div>
-        <div class="character-description">${jsonData.Plot}</div>
-        </div>`;
-  } else {
-    showContainer.innerHTML = `<p>${input.value} not found</p>`;
-  }
-});
-
-window.onload = () => {
-  // getRsult(); // You can choose whether to fetch results on page load or not
 };
+searchBtn.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
